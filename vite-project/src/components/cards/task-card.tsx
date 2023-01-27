3c33a4716aa69cb62601314499e36b29f6cbcc24
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-01-28 01:50:55 +0200

    first work whith react

--- vite-project/src/components/cards/task-card.tsx
+++ vite-project/src/components/cards/task-card.tsx
@@ -5,18 +5,18 @@ import './card.css';
 export function TaskCard () {
     return (
         <div className='card card__container'>
-        <h3>List heading</h3>
-        <ul>
-          <li>task1<button>done</button></li>
-          <li>task1<button>done</button></li>
-          <li>task1<button>done</button></li>
-          <li>task1<button>done</button></li>
-        </ul>
-        <div className="card__button-wraper">
-            <ButtonAddTask />
-            <ButtonDelete />
+            <h3>List heading</h3>
+            <ul>
+            <li>task1<button>done</button></li>
+            <li>task1<button>done</button></li>
+            <li>task1<button>done</button></li>
+            <li>task1<button>done</button></li>
+            </ul>
+            <div className="card__button-wraper">
+                <ButtonAddTask />
+                <ButtonDelete />
+            </div>
         </div>
-      </div>
     )
 }
 
